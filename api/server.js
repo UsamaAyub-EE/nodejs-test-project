99b13d2c18a2fba39b4943973a6ed3947d0962c6
usamaayubdev <usama.dar@devsinc.com>
2022-11-05 14:31:03 +0500

    Fixed DB issue:working till this point

--- api/server.js
+++ api/server.js
@@ -1,23 +1,75 @@
 const express = require('express');
-const app = express(),
-  bodyParser = require('body-parser');
-port = 3080;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-// place holder for the data
-const posts = [];
+const app = express();
 
 app.use(bodyParser.json());
 
+port = 3080;
+
+mongoose.connect('mongodb://127.0.0.1:27017/postApi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model('Post', postSchema);
+
 app.get('/api/posts', (req, res) => {
-  console.log('api/posts called!!!!');
-  res.json(posts);
+  Post.find({}, (err, post) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(post);
+      res.send(post);
+    }
+  });
+});
+
+app.get('/api/posts/:postTitle', (req, res) => {
+  Post.find({ title: req.params.postTitle }, (err, post) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(post);
+    }
+  });
 });
 
 app.post('/api/post', (req, res) => {
-  const post = req.body.post;
-  console.log('Adding post::::::::', post);
-  posts.push(post);
-  res.json('post addedd');
+  const reqPost = req.body.post;
+
+  const post = new Post({
+    title: reqPost.title ? reqPost.title : 'Blank',
+    content: reqPost.content ? reqPost.content : 'Blank',
+  });
+
+  console.log(reqPost);
+
+  post.save(err => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json('post addedd');
+    }
+  });
+});
+
+app.delete('/api/posts', (req, res) => {
+  const title = req.body.title;
+
+  Post.deleteOne({ tilte: title }, (err, post) => {
+    if (err) {
+      res.send(err);
+    }
+  });
 });
 
 app.get('/', (req, res) => {
